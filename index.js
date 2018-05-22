ec59bfdd9ee60b19491b4aa8744d673069a061f4
Randall Harris Haynes <harrishaynes@gmail.com>
2018-05-22 17:55:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const returnFirstTwoDrivers = function (arr){
   return arr.slice(0,2);
-}
+};
 
 const returnLastTwoDrivers = function (arr){
   return arr.slice(-2);
-}
+};
 
 const selectingDrivers = function(){
-}
+};
 
 const fareDoubler = function(){
-}
+};
 
 const fareTripler = function(){
-}
+};
